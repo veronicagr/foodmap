11a3ca4eb14494363645d540904110aed567cd82
veronicagr <veronica.gomes.rocha@gmail.com>
2018-08-02 22:38:35 -0300

    atualizacao

--- js/app.js
+++ js/app.js
@@ -1,1 +1,65 @@
-// Add your JavaScript
+function initMap() {
+    var uluru = { lat: -25.344, lng: 131.036 };
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 4, center: uluru });
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+};
+
+$(document).ready(function () {
+
+    $('.filtrar').click(function () {
+        var inputValue = $('.texto-input').val();
+
+        $("li").each(function () {
+            if ($(this).text() !== inputValue) {
+                $(this).fadeOut('slow');
+            }
+        });
+    })
+
+    $('.texto-input').on('input', function () {
+        if ($(this).val() === "") {
+            $("li").each(function () {
+                $(this).fadeIn('slow')
+            });
+        }
+    })
+});
+
+$.each(restaurantes, function (key, valor) {
+    console.table(restaurantes);
+
+    var nomeRestaurante = [];
+    nomeRestaurante.push("<li id='" + key + "'>" + valor.name + "</li>");
+    $(".restaurantes").append(nomeRestaurante[0]);
+
+    var latitudes = [];
+    latitudes.push(valor.latitude);
+    $(".teste").append(latitudes[0]);
+
+    var longitudes = [];
+    longitudes.push(valor.longitude);
+    $(".teste").append(longitudes[0]);
+
+    var tipo = [];
+    tipo.push("<p id = '" + key + "'>" + valor.type + "</p>");
+    $(".restaurantes").append(tipo[0]);
+
+    var descricao = [];
+    descricao.push("<p id = '" + key + "'>" + valor.description + "</p>");
+    $(".restaurantes").append(descricao[0]);
+
+    var img = [];
+    img.push("<img id='nomesRestaurantes' src ='" + valor.image + "'>");
+    $(".restaurantes").append(img[0]);
+
+
+});
+
+function filktrao(restaurantes, key) {
+    return restaurantes.map(function (restaurante) {
+        return restaurante[key];
+    })
+}
+
+
--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 const restaurantes = [
   {
     "name": "Latife",
-    "image": "../assets/latife.png",
+    "image": "assets/latife.png",
     "latitude": -23.557567,
     "longitude": -46.658615,
     "description": "O Latife Gastronomia Árabe é o primeiro Buffet por quilo especializado em Culinária Árabe em São Paulo. O cardápio do restaurante é uma continuação dos procedimentos que Dona Latife, de ascendência árabe, adotava em seu dia a dia.",
@@ -9,7 +9,7 @@ const restaurantes = [
   },
   {
     "name": "Arabesco - Paulista",
-    "image": "../assets/arabesco.jpg",
+    "image": "assets/arabesco.jpg",
     "latitude": -23.5607625,
     "longitude": -46.65784689999998,
     "description": "Arabic fare for corporate events, from salad to desserts, in a charming setting with a relaxed vibe.",
@@ -17,7 +17,7 @@ const restaurantes = [
   },
   {
     "name": "Ragazzo",
-    "image": "../assets/ragazzo.png",
+    "image": "assets/ragazzo.png",
     "latitude": -23.5588598,
     "longitude": -46.66152740000001,
     "description": "A chain of casual Italian cantinas that serves an array of pastas, pizzas, snacks & ice cream.",
@@ -25,7 +25,7 @@ const restaurantes = [
   },
   {
     "name": "McDonald's",
-    "image": "../assets/mcdonalds.png",
+    "image": "assets/mcdonalds.png",
     "latitude": -23.558783,
     "longitude": -46.66130229999999,
     "description": "Classic, long-running fast-food chain known for its burgers, fries & shakes.",
@@ -33,7 +33,7 @@ const restaurantes = [
   },
   {
     "name": "Cantina do Piero il Vero",
-    "image": "./assets/cantina.png",
+    "image": "assets/cantina.png",
     "latitude": -23.5587677,
     "longitude": -46.66278650000004,
     "description": "A traditionally decorated Italian restaurant with checkered tablecloths & sports teams' flags.",
@@ -41,7 +41,7 @@ const restaurantes = [
   },
   {
     "name": "Pop Vegan Food",
-    "image": "../assets/pop.png",
+    "image": "assets/pop.png",
     "latitude": -23.5539487,
     "longitude": -46.65767779999999,
     "description": "Restaurante vegano em São Paulo, na região da Augusta. Comida boa com preço justo e pizzas irresistíveis. Democratizando a alimentação vegana.",
@@ -49,7 +49,7 @@ const restaurantes = [
   },
   {
     "name": "Loving Hut Jardins",
-    "image": "../assets/loving.jpg",
+    "image": "assets/loving.jpg",
     "latitude": -23.5539487,
     "longitude": -46.65767779999999,
     "description": "Outpost of a vegan counter-serve chain with Asian-accented  menus that vary by location.",
@@ -57,7 +57,7 @@ const restaurantes = [
   },
   {
     "name": "Aji-To",
-    "image": "../assets/aji-to.jpg",
+    "image": "assets/aji-to.jpg",
     "latitude": -23.5560888,
     "longitude": -46.657931700000006,
     "description": "Refined restaurant with artistic Japanese décor, serving  temaki & other delicacies.",
@@ -65,7 +65,7 @@ const restaurantes = [
   },
   {
     "name": "Sushimasa",
-    "image": "../assets/sushimasa.png",
+    "image": "assets/sushimasa.png",
     "latitude": -23.5600095,
     "longitude": -46.66369069999996,
     "description": "This modern spot offers Japanese fare, with a variety of  fish, a buffet & all-you-can-eat options.",
