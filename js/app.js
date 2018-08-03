9ed9aae907ddce96b9ae8eb0332ef8e5a5c36b68
veronicagr <veronica.gomes.rocha@gmail.com>
2018-08-03 17:21:02 -0300

    atualizacao

--- js/app.js
+++ js/app.js
@@ -5,61 +5,65 @@ function initMap() {
     var marker = new google.maps.Marker({ position: uluru, map: map });
 };
 
+
+
 $(document).ready(function () {
+
+    for (restaurante of restaurantes) {
+        var img = $('<img></img>').attr('src', restaurante.image).val(restaurante.type);
+        $(".restaurantes").append(img);
+    }
+
+    // $.each(restaurantes, function (key, valor) {
+    //     console.table(restaurantes);
+
+    // var nomeRestaurante = [];
+    // nomeRestaurante.push("<li id='" + key + "'>" + valor.name + "</li>");
+    // $(".restaurantes").append(nomeRestaurante[0]);
+
+    // var latitudes = [];
+    // latitudes.push(valor.latitude);
+    // $(".teste").append(latitudes[0]);
+
+    // var longitudes = [];
+    // longitudes.push(valor.longitude);
+    // $(".teste").append(longitudes[0]);
+
+    // var tipo = [];
+    // tipo.push("<li id = '" + key + "'>" + valor.type + "</li>");
+    // $(".restaurantes").append(tipo[0]);
+
+    // var descricao = [];
+    // descricao.push("<li id = '" + key + "'>" + valor.description + "</li>");
+    // $(".restaurantes").append(descricao[0]);
+
+    //     var img = [];
+    //     img.push("<img id='nomesRestaurantes' value='" + valor.type + "' src ='" + valor.image + "'>");
+    //     $(".restaurantes").append(img);
+
+
+    // });
+
 
     $('.filtrar').click(function () {
         var inputValue = $('.texto-input').val();
 
-        $("li").each(function () {
-            if ($(this).text() !== inputValue) {
+        $("img").each(function () {
+            if ($(this).val() !== inputValue) {
                 $(this).fadeOut('slow');
+            } else {
+                $(this).fadeIn('slow');
             }
         });
     })
 
     $('.texto-input').on('input', function () {
         if ($(this).val() === "") {
-            $("li").each(function () {
+            $("img").each(function () {
                 $(this).fadeIn('slow')
             });
+
+
         }
     })
 });
-
-$.each(restaurantes, function (key, valor) {
-    console.table(restaurantes);
-
-    var nomeRestaurante = [];
-    nomeRestaurante.push("<li id='" + key + "'>" + valor.name + "</li>");
-    $(".restaurantes").append(nomeRestaurante[0]);
-
-    var latitudes = [];
-    latitudes.push(valor.latitude);
-    $(".teste").append(latitudes[0]);
-
-    var longitudes = [];
-    longitudes.push(valor.longitude);
-    $(".teste").append(longitudes[0]);
-
-    var tipo = [];
-    tipo.push("<p id = '" + key + "'>" + valor.type + "</p>");
-    $(".restaurantes").append(tipo[0]);
-
-    var descricao = [];
-    descricao.push("<p id = '" + key + "'>" + valor.description + "</p>");
-    $(".restaurantes").append(descricao[0]);
-
-    var img = [];
-    img.push("<img id='nomesRestaurantes' src ='" + valor.image + "'>");
-    $(".restaurantes").append(img[0]);
-
-
-});
-
-function filktrao(restaurantes, key) {
-    return restaurantes.map(function (restaurante) {
-        return restaurante[key];
-    })
-}
-
-
